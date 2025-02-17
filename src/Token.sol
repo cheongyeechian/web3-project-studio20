9c9329ec99ed7679c59826ab05f525aee89dfcec
cheongyeechian <cheongyeechian@gmail.com>
2025-02-17 18:09:26 +0800

    initial commit

---
+++ src/Token.sol
@@ -0,0 +1,10 @@
+// contracts/Token.sol
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// contract Token is ERC20 {
+//     constructor(uint256 initialSupply) ERC20("Studio20", 'S20') {
+//         _mint(msg.sender, initialSupply);
+// }}
